0c32e66539da7124cb7470bc1fb63c9a3587a495
ahmadazzamsprout <ahmad.azzam@sprout.co.id>
2022-08-03 17:08:28 +0700

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <div className="">
-        App
-      </div>
+      <Routes>
+
+      </Routes>
     </>
   )
 }
